c15aace266abc071acd30e26d520777e44e0c39c
Ninetta11 <ninawelsh1@outlook.com>
2024-12-19 11:21:28 +1100

    Add icons

--- crypto-watchlist/src/app/components/token.tsx
+++ crypto-watchlist/src/app/components/token.tsx
@@ -1,7 +1,9 @@
+
 export default function Token({ symbol, price, percent }: {
     symbol: string,
     price: string,
     percent: string
 }) {
-    return <li>{symbol}  ${price} {percent}%</li>
+
+    return <li><img src={`/icon/${symbol ? symbol : "generic"}.png`} /> {symbol}  ${price} {percent}%</li>
 }
--- crypto-watchlist/src/app/page.tsx
+++ crypto-watchlist/src/app/page.tsx
@@ -14,7 +14,7 @@ type Token = {
 }
 
 export default async function Home() {
-  const response = await fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=100&sort=market_cap&cryptocurrency_type=all&tag=all", {
+  const response = await fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=25&sort=market_cap&cryptocurrency_type=all&tag=all", {
     headers: {
       'X-CMC_PRO_API_KEY': process.env.COINMARKETCAPKEY ?? 'other',
     },
